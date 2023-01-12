e822149945c4dd3ca50780a134fc52b711a89ade
mgrakib <mgrakibbd@gmail.com>
2023-01-13 00:44:34 +0600

    add all  thing

---
+++ js/script.js
@@ -0,0 +1,13 @@
+const navBar = document.getElementById("navBar");
+
+
+function closeNav() {
+    navBar.style.right = "-100%";
+    // navBar.style.display = "none";
+
+}
+
+function viewNav() {
+    navBar.style.right = "0px";
+    // navBar.style.display = "block";
+}
